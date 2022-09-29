55cf0bfb05e1d9c26fd3c861fbaf9451befb2497
Виолетта Чевтайкина <lytikr@mail.ru>
2022-09-29 09:14:34 +0300

    практика 6

---
+++ pz6/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace pz6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            double m = 1;
+            int i = 1;
+            while(m<n)
+            {
+                m = Math.Pow(2, i);
+                i++;
+               if(m<n)
+                Console.WriteLine(m);
+            }
+        }
+    }
+}
