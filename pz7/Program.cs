6569dd0a543b255c2a55ccbbeeac66dfff443201
Виолетта Чевтайкина <lytikr@mail.ru>
2022-10-06 11:37:45 +0500

    pz7

---
+++ pz7/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace pz7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 10 чисел");
+            char[] numbers = new char[10];
+
+        //    
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToChar(Console.ReadLine());
+            }
+            Array.Sort(numbers);
+            foreach(char c in numbers)
+            {
+                Console.Write(c);
+            }
+            Console.WriteLine($" {numbers[9]}");
+        }
+    }
+}
